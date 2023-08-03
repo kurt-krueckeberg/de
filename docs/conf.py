57cae660713ea680c8731f42bdad48e333f1282a
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-08-03 14:11:39 -0400

    $1

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'German Pronounciation'
+copyright = '2023, Kurt'
+author = 'Kurt'
+release = '.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+myst_enable_extensions = [
+  "colon_fence", "deflist"
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+html_theme = 'sphinx_book_theme'
+
+html_static_path = ['_static']
+
+html_css_files = [
+   'custom.css',
+]
